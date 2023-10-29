6d263b64815177a5920b30d5b24f23e229860ab3
thekim123 <tacdo12@naver.com>
2023-10-29 16:02:40 +0900

    first commit

---
+++ QRCodeTutorial_Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QRCodeTutorial_Swift
+//
+//  Created by media on 2023/10/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
